98c7f5ca18d992cb934518a91648d2174ab222f9
Marius L. Jensen <funman@gmail.com>
2018-12-19 13:38:25 +0100

    Create wordpress-topics-op.meta.js

---
+++ src/wordpress-topics-op.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         WordPress.org Topics OP
+// @namespace    https://www.clorith.net
+// @version      0.1
+// @description  Mark topic OP on their replies for easy recognition.
+// @author       Clorith
+// @match        https://wordpress.org/support/topic/*
+// @match        https://*.wordpress.org/support/topic/*
+// @grant        none
+// ==/UserScript==
